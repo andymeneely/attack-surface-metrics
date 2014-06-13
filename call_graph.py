d8214c2fd009645c392c4856501fee7d8a7b3d44
Kevin Campusano <megakevinx@gmail.com>
2014-06-13 12:46:31 -0400

    Refactored away the graph generation logic and put it into a separate file.

---
+++ call_graph.py
@@ -0,0 +1,57 @@
+__author__ = 'kevin'
+
+import subprocess
+import os
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from stack import Stack
+from call import Call
+
+
+class CallGraph():
+
+    def __init__(self, source_dir):
+        self.source_dir = source_dir
+        self.call_graph = nx.DiGraph()
+
+        self.entry_points = set()
+        self.exit_points = set()
+
+        self.generate()
+
+    def generate(self):
+        i = 0
+        parent = Stack()
+        proc = subprocess.Popen(['sh', 'run_cflow.sh', self.source_dir], stdout=subprocess.PIPE)
+
+        while True:
+            line = proc.stdout.readline().decode(encoding='UTF-8')
+
+            if line == '':
+                break
+
+            current = Call(line)
+
+            if i != 0:
+                if current.level > previous.level:
+                    parent.push(previous)
+                elif current.level < previous.level:
+                    for t in range(previous.level - current.level):
+                        parent.pop()
+
+                self.call_graph.add_edge(parent.top, current)
+
+                if current.is_input_function():
+                    self.entry_points.add(parent.top)
+                if current.is_output_function():
+                    self.exit_points.add(parent.top)
+
+            previous = current
+            i += 1
+
+    def save_png(self):
+        nx.draw(self.call_graph)
+        plt.savefig(os.path.basename(os.path.normpath(self.source_dir)) + ".png")
+
--- io_points.py
+++ io_points.py
@@ -1,69 +1,16 @@
 __author__ = 'kevin'
 
 import sys
-import subprocess
-import os
 
-import networkx as nx
-import matplotlib.pyplot as plt
-
-from stack import Stack
-from call import Call
+from call_graph import CallGraph
 
 def main(args):
     source_dir = args[1]
 
-    entry_points = list()
-    exit_points = list()
-    entry_points_count = 0
-    exit_points_count = 0
+    call_graph = CallGraph(source_dir)
 
-    call_graph = nx.DiGraph()
-    parent = Stack()
-    i = 0
-
-    proc = subprocess.Popen(['sh', 'run_cflow.sh', source_dir], stdout=subprocess.PIPE)
-
-    while True:
-        line = proc.stdout.readline().decode(encoding='UTF-8')
-
-        if line == '':
-            break
-
-        print(str(i) + ":\t" + line.rstrip())
-        current = Call(line)
-
-        if i != 0:
-            if current.level > previous.level:
-                parent.push(previous)
-            elif current.level < previous.level:
-                for t in range(previous.level - current.level):
-                    parent.pop()
-
-            call_graph.add_edge(parent.top, current)
-
-            if current.is_input_function():
-                entry_points.append(parent.top)
-                # entry_points_count += 1
-            if current.is_output_function():
-                exit_points.append(parent.top)
-                # exit_points_count += 1
-
-        previous = current
-        i += 1
-
-    # print("entry points: " + str(entry_points_count))
-    # print("exit points: " + str(exit_points_count))
-
-    print("entry points: " + str(len(entry_points)))
-    print("exit points: " + str(len(exit_points)))
-
-    print("entry points: " + str(len(set(entry_points))))
-    print("exit points: " + str(len(set(exit_points))))
-
-    nx.draw(call_graph)
-    plt.savefig(os.path.basename(os.path.normpath(source_dir)) + ".png")
-
+    print("entry points: " + str(len(call_graph.entry_points)))
+    print("exit points: " + str(len(call_graph.exit_points)))
 
 if __name__ == '__main__':
     main(sys.argv)
