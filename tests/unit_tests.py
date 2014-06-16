eeb88381806575a3b98ec5f63eeeb855e684288a
Kevin Campusano <megakevinx@gmail.com>
2014-06-16 14:26:43 -0400

    Wrote some unit tests for the Stack and Call classes.

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,12 +1,193 @@
 __author__ = 'kevin'
 
 import unittest
+from attack_surface.stack import Stack
+from attack_surface.call import Call
+from attack_surface.call_graph import CallGraph
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class StackTestCase(unittest.TestCase):
+
+    def test_push(self):
+        # Arrange
+        test_stack = Stack()
+
+        # Act
+        test_stack.push(1)
+        test_stack.push(2)
+
+        # Assert
+        self.assertEqual(len(test_stack), 2)
+
+    def test_pop(self):
+        # Arrange
+        test_stack = Stack()
+
+        # Act
+        test_stack.push(1)
+        test_stack.push(2)
+
+        popped_value = test_stack.pop()
+
+        # Assert
+        self.assertEqual(popped_value, 2)
+        self.assertEqual(len(test_stack), 1)
+
+    def test_top(self):
+        # Arrange
+        test_stack = Stack()
+
+        # Act
+        test_stack.push(1)
+        test_stack.push(2)
+
+        # Assert
+        self.assertEqual(test_stack.top, 2)
+
+
+class CallTestCase(unittest.TestCase):
+
+    def test_identity_function_name(self):
+        # Arrange
+        cflow_line = "printf()"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertEqual(test_call.identity, "printf()")
+
+    def test_identity_full(self):
+        # Arrange
+        cflow_line = "xstrdup() <char *xstrdup (const char *str) at ./cyrus/lib/xmalloc.c:89> (R):"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertEqual(test_call.identity, "xstrdup()<char *xstrdup (const char *str) at ./cyrus/lib/xmalloc.c:89>")
+
+    def test_function_name_only_name(self):
+        # Arrange
+        cflow_line = "printf()"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertEqual(test_call.function_name, "printf()")
+
+    def test_function_name_full(self):
+        # Arrange
+        cflow_line = "xstrdup() <char *xstrdup (const char *str) at ./cyrus/lib/xmalloc.c:89> (R):"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertEqual(test_call.function_name, "xstrdup()")
+
+    def test_function_signature_only_name(self):
+        # Arrange
+        cflow_line = "printf()"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertIsNone(test_call.function_signature)
+
+    def test_function_signature_full(self):
+        # Arrange
+        cflow_line = "xstrdup() <char *xstrdup (const char *str) at ./cyrus/lib/xmalloc.c:89> (R):"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertEqual(test_call.function_signature, "<char *xstrdup (const char *str) at ./cyrus/lib/xmalloc.c:89>")
+
+    def test_is_leaf(self):
+        # Arrange
+        cflow_line = "printf()"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertTrue(test_call.is_leaf())
+
+    def test_is_not_leaf(self):
+        # Arrange
+        cflow_line = "xstrdup() <char *xstrdup (const char *str) at ./cyrus/lib/xmalloc.c:89> (R):"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertFalse(test_call.is_leaf())
+
+    def test_is_input_function(self):
+        # Arrange
+        cflow_line = "getchar()"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertTrue(test_call.is_input_function())
+
+    def test_is_not_input_function(self):
+        # Arrange
+        cflow_line = "printf()"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertFalse(test_call.is_input_function())
+
+    def test_is_not_input_function_no_leaf(self):
+        # Arrange
+        cflow_line = "xstrdup() <char *xstrdup (const char *str) at ./cyrus/lib/xmalloc.c:89> (R):"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertFalse(test_call.is_input_function())
+
+    def test_is_output_function(self):
+        # Arrange
+        cflow_line = "printf()"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertTrue(test_call.is_output_function())
+
+    def test_is_not_output_function(self):
+        # Arrange
+        cflow_line = "getchar()"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertFalse(test_call.is_output_function())
+
+    def test_is_not_output_function_no_leaf(self):
+        # Arrange
+        cflow_line = "xstrdup() <char *xstrdup (const char *str) at ./cyrus/lib/xmalloc.c:89> (R):"
+        test_call = Call(cflow_line)
+
+        # Assert
+        self.assertFalse(test_call.is_output_function())
+
+    def test_equal(self):
+        # Arrange
+        cflow_line = "getchar()"
+        test_call_1 = Call(cflow_line)
+        test_call_2 = Call(cflow_line)
+
+        # Assert
+        self.assertEqual(test_call_1, test_call_2)
+
+    def test_not_equal(self):
+        # Arrange
+        cflow_line_1 = "getchar()"
+        cflow_line_2 = "xstrdup() <char *xstrdup (const char *str) at ./cyrus/lib/xmalloc.c:89> (R):"
+        test_call_1 = Call(cflow_line_1)
+        test_call_2 = Call(cflow_line_2)
+
+        # Assert
+        self.assertNotEqual(test_call_1, test_call_2)
+
+
+class CallGraphTestCase(unittest.TestCase):
+    # Arrange
+    test_call_graph = CallGraph("asdasd")
+
+    # Act
+    # Assert
+
 
 
 if __name__ == '__main__':
     unittest.main()
+
