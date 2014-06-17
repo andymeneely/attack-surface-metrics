6d8cf5dc8d5fa3e3c26e719129e7f761aefed8de
Kevin Campusano <megakevinx@gmail.com>
2014-06-17 12:06:50 -0400

    Added files for packaging.

---
+++ setup.py
@@ -0,0 +1,14 @@
+__author__ = 'kevin'
+
+from distutils.core import setup
+
+setup(
+    name='AttackSurfaceMeter',
+    version='0.0.1-dev',
+    packages=['attack_surface',],
+    license='The MIT License (MIT) Copyright (c) 2014 Andy Meneely',
+    long_description=open('README').read(),
+    author='Kevin Campusano',
+    author_email='kac2375@rit.edu',
+    url='https://github.com/andymeneely/attack-surface-metrics'
+)
