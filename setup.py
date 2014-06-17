14ac1500bce8322afde918407fc443f76d4a7efa
Kevin Campusano <megakevinx@gmail.com>
2014-06-17 15:58:30 -0400

    Some fixes so that uploaded package can be installed using pip.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(
     name='AttackSurfaceMeter',
-    version='0.0.1-dev',
+    version='0.0.1',
     packages=['attack_surface',],
     license='The MIT License (MIT) Copyright (c) 2014 Andy Meneely',
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
     author='Kevin Campusano',
     author_email='kac2375@rit.edu',
     url='https://github.com/andymeneely/attack-surface-metrics'
