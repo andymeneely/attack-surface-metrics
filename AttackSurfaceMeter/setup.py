10102750d4a2bcf1f273451b0d8a33a5d5f70120
Kevin Campusano <megakevinx@gmail.com>
2014-06-18 18:43:13 -0400

    Modified position of the description field in setup.py

--- AttackSurfaceMeter/setup.py
+++ AttackSurfaceMeter/setup.py
@@ -4,12 +4,11 @@ from distutils.core import setup
 
 setup(
     name='AttackSurfaceMeter',
-    description='Library for collecting metrics of the attack surface.',
     version='0.0.1',
     packages=['attacksurfacemeter',],
     requires=['networkx', 'matplotlib'],
     license='The MIT License (MIT) Copyright (c) 2014 Andy Meneely',
-    description='Scripts for collecting metrics of the attack surface',
+    description='Library for collecting metrics of the attack surface.',
     long_description=open('README.md').read(),
     author='Kevin Campusano',
     author_email='kac2375@rit.edu',
