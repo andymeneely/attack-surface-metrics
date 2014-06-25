ec144198cf2b63630eca015ed5dba9bb9c4e97fd
Kevin Campusano <megakevinx@gmail.com>
2014-06-25 10:53:53 -0400

    Printing to console all the stats extracted from the call graph so far.

--- AttackSurfaceMeter/main.py
+++ AttackSurfaceMeter/main.py
@@ -10,8 +10,18 @@ def main(args):
 
     call_graph = CallGraph(source_dir)
 
+    print("nodes: " + str(len(call_graph.nodes)))
+    print("edges: " + str(len(call_graph.edges)))
+
     print("entry points: " + str(len(call_graph.entry_points)))
     print("exit points: " + str(len(call_graph.exit_points)))
+
+    print("execution paths: " + str(len(call_graph.execution_paths)))
+    print("average execution path length: " + str(call_graph.avg_execution_path_length))
+    print("median execution path length: " + str(call_graph.median_execution_path_length))
+
+    print("average entry point clustering: " + str(call_graph.entry_points_clustering))
+    print("average exit point clustering: " + str(call_graph.exit_points_clustering))
 
 if __name__ == '__main__':
     main(sys.argv)
