92a3266a7ebaf4f95e90a888094cd6d7fed13f23
Kevin Campusano <megakevinx@gmail.com>
2014-06-25 15:35:34 -0400

    Added function pointers to C sample.

--- AttackSurfaceMeter/tests/helloworld/src/helloworld.c
+++ AttackSurfaceMeter/tests/helloworld/src/helloworld.c
@@ -15,12 +15,62 @@
 void greet_a(int);
 void greet_b(int);
 
+int addInt(int n, int m)
+{
+    return n+m;
+}
+
+int (*functionPtr)(int,int);
+
+
+typedef struct Greeter_Struct* Greeter;
+
+struct Greeter_Struct
+{
+	void (*SayHi)();
+	void (*SayHiTo)(int value);
+};
+
+void GreeterSayHi();
+void GreeterSayHiTo(int value);
+
+Greeter new_Greeter();
+
+Greeter new_Greeter()
+{
+	Greeter self = (Greeter)malloc(sizeof(struct Greeter_Struct));
+
+    self->SayHi = &GreeterSayHi;
+    self->SayHiTo = &GreeterSayHiTo;
+
+    return self;
+}
+
+void GreeterSayHi()
+{
+    printf("Hello from greeter!!");
+}
+
+void GreeterSayHiTo(int value)
+{
+    printf("Hello %d from greeter!!", value);
+}
+
 int main(void)
 {
 	greet_a(5);
 	greet_b(10);
 
 	puts("lol");
+
+	functionPtr = &addInt;
+	int sum = (*functionPtr)(2, 3); // sum == 5
+
+	Greeter g = new_Greeter();
+	g->SayHi();
+	g->SayHiTo(100);
+
+	printf("---> %d", sum);
 
 	return EXIT_SUCCESS;
 }
@@ -34,7 +84,7 @@ void greet_a(int i)
 void greet_b(int i)
 {
 	char c;
-	gets(c);
+	//gets(c);
 
 	greet(0);
 	recursive_b(i);
