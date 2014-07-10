21487238a54d16cb16f35cbd700cc958dccef405
Kevin Campusano <megakevinx@gmail.com>
2014-07-10 19:58:08 -0400

    Added docstring documentation for the Stack class and all it's methods.

--- AttackSurfaceMeter/attacksurfacemeter/stack.py
+++ AttackSurfaceMeter/attacksurfacemeter/stack.py
@@ -2,21 +2,51 @@ __author__ = 'kevin'
 
 
 class Stack():
+    """
+        Simple Stack (FILO collection) implementation based on python's list.
+    """
 
     def __init__(self):
+        """
+            Stack constructor.
+
+            Returns:
+                A new instance of Stack.
+        """
         self._collection = list()
 
     def __len__(self):
+        """
+            Returns the number of objects contained in the sSack.
+
+            Returns:
+                An Int representing the number of objects contained in the Stack.
+        """
         return len(self._collection)
 
     def push(self, data):
+        """
+            Adds an object at the top of the Stack.
+
+            Args:
+                data: The object that will be added at the top of the Stack.
+        """
         self._collection.append(data)
 
     def pop(self):
+        """
+            Removes the object at the top of the Stack and returns it.
+
+            Returns:
+                The object at the top of the Stack.
+        """
         return self._collection.pop()
 
     @property
     def top(self):
+        """
+            Returns the object at the top of the Stack.
+        """
         if self._collection:
             return self._collection[-1]
         else:
