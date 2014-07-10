d8afeb51ca4ea0adbc5da10d892d3e427c451d1d
Kevin Campusano <megakevinx@gmail.com>
2014-07-10 14:14:32 -0400

    Added docstring documentation to CallGraph and some of it's methods.

--- AttackSurfaceMeter/attacksurfacemeter/call_graph.py
+++ AttackSurfaceMeter/attacksurfacemeter/call_graph.py
@@ -11,8 +11,31 @@ from attacksurfacemeter.call import Call
 
 
 class CallGraph():
+    """
+        Represents a the Call Graph of a software system.
+
+        Encapsulates a graph data structure where each node is a method/function call.
+
+        Attributes:
+            source_dir: String that contains the root directory of the source code that this Call Graph represents.
+            call_graph: networkx.DiGraph. Internal representation of the graph data structure.
+    """
 
     def __init__(self, source_dir, reverse=False):
+        """
+            CallGraph constructor
+
+            Instantiates a new CallGraph object and generates a networkx.DiGraph representing the Call Graph of
+            the source code located an the supplied source_dir.
+
+            Args:
+                source_dir: String that contains the root directory of the source code to generate the Call Graph for.
+                reverse: Boolean specifying whether the graph generation software (cflow) should use the reverse
+                    algorithm.
+
+            Returns:
+                A new instance of type CallGraph.
+        """
         self.source_dir = source_dir
         self.call_graph = nx.DiGraph()
 
@@ -24,6 +47,19 @@ class CallGraph():
         self._generate(reverse)
 
     def _generate(self, is_reverse):
+        """
+            Generates the Call Graph as a networkx.DiGraph object.
+
+            Invokes the call grap generation software (cflow) and creates a networkx.DiGraph instance that represents
+            the analyzed source code's Call Graph.
+
+            Args:
+                is_reverse: Boolean specifying whether the graph generation software (cflow) should use the reverse
+                    algorithm.
+
+            Returns:
+                None
+        """
         is_first_line = True
         parent = Stack()
 
@@ -54,6 +90,16 @@ class CallGraph():
             is_first_line = False
 
     def _exec_cflow(self, is_reverse):
+        """
+            Creates a subprocess.Popen instance representing the cflow call.
+        
+            Args:
+                is_reverse: Boolean specifying whether the graph generation software (cflow) should use the reverse
+                    algorithm.
+                
+            Returns:
+                A subprocess.Popen instance representing the cflow call.
+        """
         if is_reverse:
             cflow_exe = 'run_cflow_r.sh'
         else:
@@ -66,10 +112,38 @@ class CallGraph():
         return proc
 
     def _select_nodes(self, predicate):
+        """
+            Selects the nodes in the graph for which predicate is True.
+
+            Args:
+                predicate: Function to use to determine which nodes to return from the entire Call Graph.
+                    This function must receive a Call object as a parameter and return a Boolean that says
+                    if the node meets the selection criteria.
+
+            Returns:
+                A List of all the nodes in the cal graph for which predicate is true.
+        """
         return [n for n in self.call_graph.nodes() if predicate(n)]
 
     @property
     def entry_points(self):
+        """
+            Returns all the Entry Points present in the Call Graph.
+        
+            Entry Points are functions that contain calls to input functions in the standard library.
+            
+            This is the list of the functions that are considered input functions in the C standard library:
+            
+            canonicalize_file_name, catgets, confstr, ctermid, ctermid, cuserid, dgettext, dngettext, fgetc,
+            fgetc_unlocked, fgets, fgets_unlocked, fpathconf, fread, fread_unlocked, fscanf, getc, getchar,
+            getchar_unlocked, getc_unlocked, get_current_dir_name, getcwd, getdelim, __getdelim, getdelim, getdents,
+            getenv, gethostbyaddr, gethostbyname, gethostbyname2, gethostent, gethostid, getline, getline, getlogin,
+            getlogin_r, getmsg, getopt, _getopt_internal, getopt_long, getopt_long_only, getpass, getpmsg, gets,
+            gettext, getw, getwd, ngettext, pathconf, pread, pread64, ptsname, ptsname_r, read, readdir, readlink,
+            readv, realpath, recv, recv_from, recvmesg, scanf, __secure_getenv, signal, sysconf, ttyname, ttyname_r,
+            vfscanf, vscanf
+            
+        """
         if not self._entry_points:
             self._entry_points = self._select_nodes(lambda n: any([s.is_input_function() for s
                                                                    in self.call_graph.successors(n)]))
@@ -77,6 +151,19 @@ class CallGraph():
 
     @property
     def exit_points(self):
+        """
+            Returns all the Exit Points present in the Call Graph.
+        
+            Exit Points are functions that contain calls to output functions in the standard library.
+            
+            This is the list of the functions that are considered output functions in the C standard library:
+            
+            dprintf, fprintf, fputc, fputchar_unlocked, fputc_unlocked, fputs, fputs_unlocked, fwrite, fwrite_unlocked,
+            perror, printf, psignal, putc, putchar, putc_unlocked, putenv, putmsg, putpmsg, puts, putw, pwrite,
+            pwrite64, send, sendmsg, sendto, setenv, sethostid, setlogin, ungetc, vdprintf, vfprintf, vsyslog, write,
+            writev
+            
+        """
         if not self._exit_points:
             self._exit_points = self._select_nodes(lambda n: any([s.is_output_function() for s
                                                                   in self.call_graph.successors(n)]))
@@ -84,22 +171,61 @@ class CallGraph():
 
     @property
     def nodes(self):
+        """
+            Returns all the nodes present in the Call Graph.
+
+            Each node is a Call object with the information of a specific function/method call.
+        """
         return self.call_graph.nodes()
 
     @property
     def edges(self):
+        """
+            Returns all the edges present in the Call Graph.
+
+            Each edge is a 2-tuple of Call objects where the first member represents the caller and the second
+            member represents the callee.
+        """
         return self.call_graph.edges()
 
     def save_png(self):
+        """
+            Creates a graphical representation of the Call Graph and saves it.
+
+            The created image file is a png and is saved in the current directory with the name of the folder where
+            the source code for which the Call Graph was generated is located.
+        """
         nx.draw(self.call_graph)
         plt.savefig(os.path.basename(os.path.normpath(self.source_dir)) + ".png")
         plt.clf()
     
     def shortest_path(self, source, target):
+        """
+            Computes the shortest path between source and target.
+
+            Args:
+                source: The start point of the calculated path.
+                
+            Returns:
+                A List of nodes representing the path from source to target.
+                
+            Raises:
+                networkx.exception.NetworkXNoPath: There is no path between source and target.
+                networkx.exception.NetworkXError: source or target or both are not present in the Call Graph.
+        """
         return nx.shortest_path(self.call_graph, source, target)
 
     @property
     def execution_paths(self):
+        """
+            Returns all the Execution Paths present in the Call Graph.
+
+            An Execution Path is every possible path in the Call Graph that begins in an Entry Point and ends
+            in an Exit Point.
+
+            Returns:
+                A List where each element is a List of Call objects representing the paths in the Call Graph.
+        """
         if not self._execution_paths:
             paths = []
 
@@ -113,49 +239,168 @@ class CallGraph():
         return self._execution_paths
 
     def get_execution_paths_for(self, call):
+        """
+            Returns all the Execution Paths in which call appears.
+
+            Args:
+                call: A Call object for which Execution Paths want to be found.
+
+            Returns:
+                A List where each element is a List of Call objects representing the paths in the Call Graph that
+                call is part of.
+        """
         return [path for path in self.execution_paths if call in path]
 
     @property
     def avg_execution_path_length(self):
+        """
+            Returns the average length of all the Execution Paths in the Call Graph.
+
+            Returns:
+                An Float representing the calculated value.
+        """
         return stat.mean([len(p) for p in self.execution_paths])
 
     @property
     def median_execution_path_length(self):
+        """
+            Returns the median length of all the Execution Paths in the Call Graph.
+
+            Returns:
+                An Float representing the calculated value.
+        """
         return stat.median([len(p) for p in self.execution_paths])
 
     def _get_distances(self, distance_calculator, call, paths=None):
+        """
+            Returns tne distance from call to a given point in the provided or all of the Execution Paths.
+
+            Args:
+                distance_calculator: Function that will be used to calculate the distance. The function must recieve
+                    a Call instance as a first parameter and an Execution Path (List of Calls) as the second parameter
+                    and return an Int that represents the calculated distance.
+                call: A Call instance for which the distances will be calculated.
+                paths: Optional List of List of Call instances that represent the Execution Paths in which call's
+                    distance will be calculated. If paths is not provided, the distances will be calculated for all
+                    the Execution Paths where call appears. If call is not present in an Execution Path, the distance
+                    for that specific path will be None.
+
+            Returns:
+                A List of Dictionaries with a 'path' member that contains an Execution Path and a 'distance' member
+                that contains the distance calculated for call for the Execution Path contained in the 'path' member.
+
+        """
         distances = list()
 
         if paths:
             paths_to_search = paths
         else:
-            paths_to_search = self.execution_paths
+            paths_to_search = self.get_execution_paths_for(call)
 
         for path in paths_to_search:
-            distance_to_point = distance_calculator(call, path)
+            try:
+                distance_to_point = distance_calculator(call, path)
+            except:
+                distance_to_point = None
 
             distances.append({'path': path, 'distance': distance_to_point})
 
         return distances
 
     def get_distance_to_entry_point(self, call, paths=None):
+        """
+            Returns the distance of call to the Entry Point in the provided or all of the Execution Paths.
+
+            Args:
+                call: A Call instance for which the distances will be calculated.
+                paths: Optional List of List of Call instances that represent the Execution Paths in which call's
+                    distance to the Entry Point will be calculated. If call is not present in an Execution Path
+                    provided in paths, the distance for that specific path will be None. If paths is not provided,
+                    the distances will be calculated for all the Execution Paths where call appears.
+
+            Returns:
+                A List of Dictionaries with a 'path' member that contains an Execution Path and a 'distance' member
+                that contains the distance of call to the Entry Point in the Execution Path contained in the
+                'path' member.
+        """
         calculator = lambda c, p: p.index(c)
         return self._get_distances(calculator, call, paths)
 
     def get_distance_to_exit_point(self, call, paths=None):
+        """
+            Returns the distance of call to the Exit Point in the provided or all of the Execution Paths.
+
+            Args:
+                call: A Call instance for which the distances will be calculated.
+                paths: Optional List of List of Call instances that represent the Execution Paths in which call's
+                    distance to the Exit Point will be calculated. If call is not present in an Execution Path
+                    provided in paths, the distance for that specific path will be None. If paths is not provided,
+                    the distances will be calculated for all the Execution Paths where call appears.
+
+            Returns:
+                A List of Dictionaries with a 'path' member that contains an Execution Path and a 'distance' member
+                that contains the distance of call to the Exit Point in the Execution Path contained in the
+                'path' member.
+        """
         calculator = lambda c, p: len(p) - p.index(c) - 1
         return self._get_distances(calculator, call, paths)
 
     def get_closeness(self, call=None):
+        """
+            Calculates the Closeness of call.
+
+            The Closeness of a node represents how close it is to all the other nodes in a graph. It is roughly
+            the average of the lengths of all the shortest paths from a specific node to all other nodes.
+            Closeness is defined more formally in:
+
+            http://networkx.github.io/documentation/networkx-1.9/reference/generated/
+            networkx.algorithms.centrality.closeness_centrality.html
+
+            and
+
+            Freeman, L.C., 1979. Centrality in networks: I. Conceptual clarification. Social Networks 1, 215–239.
+            http://leonidzhukov.net/hse/2013/socialnetworks/papers/freeman79-centrality.pdf
+
+            Args:
+                call: An optional Call instance for which the Closeness will be calculated. If call is not provided,
+                    Closeness will be calculated for every Call in the Call Graph.
+
+            Returns:
+                If call is provided, returns a Float that represents the Closeness of call. If not, returns a
+                dictionary with every Call in the Call Graph with their respective Closenesses.
+        """
         return nx.closeness_centrality(self.call_graph, call)
     
     def get_betweenness(self, call=None):
+        """
+            Calculates the Betweenness of call.
+
+            The Betweenness of a node represents how between it is to all the other pairs of nodes in a graph. It is
+            roughly the proportion of how many of all the shortest paths between every pair of nodes pass through a
+            given node. Betweenness is defined more formally in:
+
+            http://networkx.github.io/documentation/networkx-1.9/reference/generated/
+            networkx.algorithms.centrality.betweenness_centrality.html
+
+            and
+
+            Brandes U., 2001, A Faster Algorithm for Betweenness Centrality. Journal of Mathematical Sociology 25(2):163-177.
+            http://www.inf.uni-konstanz.de/algo/publications/b-fabc-01.pdf
+
+            Args:
+                call: An optional Call instance for which the Betweenness will be calculated. If call is not provided,
+                    Betweenness will be calculated for every Call in the Call Graph.
+
+            Returns:
+                If call is provided, returns a Float that represents the Betweenness of call. If not, returns a
+                dictionary with every Call in the Call Graph with their respective Betweennesses.
+        """
         betweennesses = nx.betweenness_centrality(self.call_graph)
 
         if call:
             return betweennesses[call]
         else:
-            return nx.betweenness_centrality(self.call_graph)
+            return betweennesses
 
     def average_clustering(self, nodes):
         return nx.average_clustering(self.call_graph.to_undirected(), nodes)
