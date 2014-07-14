f79a94897d192ec8b5390eabb9362e691e791511
Kevin Campusano <megakevinx@gmail.com>
2014-07-14 17:00:09 -0400

    Improved on the function that prints the attack surface report to console. It is pretty much complete but needs a good refactor so new output formats can be incorporated.

--- AttackSurfaceMeter/main.py
+++ AttackSurfaceMeter/main.py
@@ -14,8 +14,6 @@ def main():
         output_as_txt(call_graph)
 
 
-
-
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Analyzes a software's source code and reports various metrics related to it's attack surface.")
@@ -86,27 +84,186 @@ def output_as_txt(call_graph):
     print()
     print("Average Execution Path Length: " + str(call_graph.avg_execution_path_length))
     print("Median Execution Path Length: " + str(call_graph.median_execution_path_length))
+    print()
+
+    print("Closeness")
+    print("=====================")
+    print()
+    closeness = "\n".join([k.function_name + ": " + str(v)
+                           for k, v in call_graph.get_closeness().items()])
+    print(closeness)
+    print()
+
+    print("Betweenness")
+    print("=====================")
+    print()
+    betweenness = "\n".join([k.function_name + ": " + str(v)
+                             for k, v in call_graph.get_betweenness().items()])
+    print(betweenness)
+    print()
 
     print("Clustering")
     print("=====================")
     print()
     print("Average Entry Point Clustering: " + str(call_graph.entry_points_clustering))
     print("Average Exit Point Clustering: " + str(call_graph.exit_points_clustering))
+    print()
+
+    print("Degree Centrality")
+    print("=====================")
+    print()
+    centrality = "\n".join([k.function_name + ": " + str(v)
+                            for k, v in call_graph.get_degree_centrality().items()])
+    print(centrality)
+    print()
+
+    print("In Degree Centrality")
+    print("=====================")
+    print()
+    in_centrality = "\n".join([k.function_name + ": " + str(v)
+                               for k, v in call_graph.get_in_degree_centrality().items()])
+    print(in_centrality)
+    print()
+
+    print("Out Degree Centrality")
+    print("=====================")
+    print()
+    out_centrality = "\n".join([k.function_name + ": " + str(v)
+                                for k, v in call_graph.get_out_degree_centrality().items()])
+    print(out_centrality)
+    print()
+
+    print("Degree")
+    print("=====================")
+    print()
+    degree = "\n".join([k.function_name + ": " + str(v)
+                        for k, v in call_graph.get_degree().items()])
+    print(degree)
+    print()
+
+    print("In Degree")
+    print("=====================")
+    print()
+    degree = "\n".join([k.function_name + ": " + str(v)
+                        for k, v in call_graph.get_in_degree().items()])
+    print(degree)
+    print()
+
+    print("Out Degree")
+    print("=====================")
+    print()
+    degree = "\n".join([k.function_name + ": " + str(v)
+                        for k, v in call_graph.get_out_degree().items()])
+    print(degree)
+    print()
+
+    # print("Descendants")
+    # print("=====================")
+    # print()
+    # descendants = "\n".join([c.function_name + ": " +
+    #                          ", ".join([d.function_name
+    #                                     for d in call_graph.get_descendants(c)])
+    #                          for c in call_graph.nodes
+    #                          if len(call_graph.get_descendants(c)) > 0])
+    # print(descendants)
+    # print()
+
+    print("Descendant Entry Points")
+    print("=====================")
+    print()
+    descendants = "\n".join([c.function_name + ": " +
+                             ", ".join([d.function_name
+                                        for d in call_graph.get_descendant_entry_points(c)])
+                             for c in call_graph.nodes
+                             if len(call_graph.get_descendant_entry_points(c)) > 0])
+    print(descendants)
+    print()
+
+    print("Descendant Exit Points")
+    print("=====================")
+    print()
+    descendants = "\n".join([c.function_name + ": " +
+                             ", ".join([d.function_name
+                                        for d in call_graph.get_descendant_exit_points(c)])
+                             for c in call_graph.nodes
+                             if len(call_graph.get_descendant_exit_points(c)) > 0])
+    print(descendants)
+    print()
+
+    # print("Ancestors")
+    # print("=====================")
+    # print()
+    # ancestors = "\n".join([c.function_name + ": " +
+    #                        ", ".join([d.function_name
+    #                                   for d in call_graph.get_ancestors(c)])
+    #                        for c in call_graph.nodes
+    #                        if len(call_graph.get_ancestors(c)) > 0])
+    # print(ancestors)
+    # print()
+
+    print("Ancestor Entry Points")
+    print("=====================")
+    print()
+    ancestors = "\n".join([c.function_name + ": " +
+                           ", ".join([d.function_name
+                                      for d in call_graph.get_ancestor_entry_points(c)])
+                           for c in call_graph.nodes
+                           if len(call_graph.get_ancestor_entry_points(c)) > 0])
+    print(ancestors)
+    print()
+
+    print("Ancestor Exit Points")
+    print("=====================")
+    print()
+    ancestors = "\n".join([c.function_name + ": " +
+                           ", ".join([d.function_name
+                                      for d in call_graph.get_ancestor_exit_points(c)])
+                           for c in call_graph.nodes
+                           if len(call_graph.get_ancestor_exit_points(c)) > 0])
+    print(ancestors)
+    print()
+
+    print("Descendant Entry Points Ratio")
+    print("=====================")
+    print()
+    descendants = "\n".join([c.function_name + ": " +
+                             str(call_graph.get_descendants_entry_point_ratio(c))
+                             for c in call_graph.nodes])
+    print(descendants)
+    print()
+
+    print("Descendant Exit Points Ratio")
+    print("=====================")
+    print()
+    descendants = "\n".join([c.function_name + ": " +
+                             str(call_graph.get_descendants_exit_point_ratio(c))
+                             for c in call_graph.nodes])
+    print(descendants)
+    print()
+
+    print("Ancestor Entry Points Ratio")
+    print("=====================")
+    print()
+    ancestors = "\n".join([c.function_name + ": " +
+                           str(call_graph.get_ancestors_entry_point_ratio(c))
+                           for c in call_graph.nodes])
+    print(ancestors)
+    print()
+
+    print("Ancestor Exit Points Ratio")
+    print("=====================")
+    print()
+    ancestors = "\n".join([c.function_name + ": " +
+                           str(call_graph.get_ancestors_exit_point_ratio(c))
+                           for c in call_graph.nodes])
+    print(ancestors)
+    print()
 
 
-def calculate_metrics(call_graph):
-    print("nodes: " + str(len(call_graph.nodes)))
-    print("edges: " + str(len(call_graph.edges)))
 
-    print("entry points: " + str(len(call_graph.entry_points)))
-    print("exit points: " + str(len(call_graph.exit_points)))
 
-    print("execution paths: " + str(len(call_graph.execution_paths)))
-    print("average execution path length: " + str(call_graph.avg_execution_path_length))
-    print("median execution path length: " + str(call_graph.median_execution_path_length))
 
-    print("average entry point clustering: " + str(call_graph.entry_points_clustering))
-    print("average exit point clustering: " + str(call_graph.exit_points_clustering))
+
 
 
 if __name__ == '__main__':
