4f5f58d3e4cdf1c8f1f17ee26d85c41651cfd30f
Kevin Campusano <megakevinx@gmail.com>
2014-07-15 17:38:55 -0400

    Included nodes, edges, entry points, exit points and execution paths in xml output format.

--- AttackSurfaceMeter/formatters/xml_formatter.py
+++ AttackSurfaceMeter/formatters/xml_formatter.py
@@ -5,25 +5,63 @@ from xml.dom.minidom import parseString
 
 from formatters.base_formatter import BaseFormatter
 
+# TODO: Refactor out of this class and into BaseFormatter all the
+# code that accesses call_graph properties and functions.
+# Ideally, derived formatter classes will send lambdas to
+# their base class's methods that will serve as selectors
+# for the various collections.
+
 
 class XmlFormatter(BaseFormatter):
     def __init__(self, call_graph):
         super(XmlFormatter, self).__init__(call_graph)
 
     def write_output(self):
-        root = XElement("attacksurface",
+        root = XElement("attack_surface",
                         {'directory': self.call_graph.source_dir},
                         XElement("nodes",
                                  {'count': str(len(self.call_graph.nodes))},
-                                 [XElement(c.function_name) for c in self.call_graph.nodes]),
-                        XElement("whatever"))
+                                 [self.call_to_xml(c) for c in self.call_graph.nodes]),
+                        XElement("edges",
+                                 {'count': str(len(self.call_graph.edges))},
+                                 [XElement('edge',
+                                           {'from': f.function_name, 'to': t.function_name})
+                                  for (f, t) in self.call_graph.edges]),
+                        XElement('entry_points',
+                                 {'count': str(len(self.call_graph.entry_points))},
+                                 [self.call_to_xml(c) for c in self.call_graph.entry_points]),
+                        XElement('exit_points',
+                                 {'count': str(len(self.call_graph.exit_points))},
+                                 [self.call_to_xml(c) for c in self.call_graph.exit_points]),
+                        XElement('execution_paths',
+                                 {'count': str(len(self.call_graph.execution_paths))},
+                                 [XElement('path',
+                                           {'length': str(len(xp))},
+                                           xp)
+                                  for xp in [[self.call_to_xml(c) for c in p]
+                                             for p in self.call_graph.execution_paths]])
+        )
 
         print(self.prettyfy(root))
 
     def prettyfy(self, xml_element):
+        """
+
+            Args:
+                :
+
+            Returns:
+
+
+            Raises:
+                :
+        """
         return parseString(
             Xml.tostring(xml_element, encoding="unicode")
         ).toprettyxml()
+
+    def call_to_xml(self, call):
+        return XElement('call', {'function_name': call.function_name})
 
 
 class XElement(Xml.Element):
