51ee93b9722b421ca3572edf5d1fb43e711041d8
Kevin Campusano <megakevinx@gmail.com>
2014-07-16 15:13:07 -0400

    Modified xml output.

--- AttackSurfaceMeter/formatters/xml_formatter.py
+++ AttackSurfaceMeter/formatters/xml_formatter.py
@@ -21,7 +21,35 @@ class XmlFormatter(BaseFormatter):
                         {'directory': self.call_graph.source_dir},
                         XElement("nodes",
                                  {'count': str(len(self.call_graph.nodes))},
-                                 [self.call_to_xml(c) for c in self.call_graph.nodes]),
+                                 [XElement('call',
+                                           {
+                                               'function_name': c.function_name,
+                                               'closeness': str(self.call_graph.get_closeness(c)),
+                                               'betweenness': str(self.call_graph.get_betweenness(c)),
+                                               'degree_centrality': str(self.call_graph.get_degree_centrality(c)),
+                                               'in_degree_centrality': str(self.call_graph.get_in_degree_centrality(c)),
+                                               'out_degree_centrality': str(self.call_graph.get_out_degree_centrality(c)),
+                                               'degree': str(self.call_graph.get_degree(c)),
+                                               'in_degree': str(self.call_graph.get_in_degree(c)),
+                                               'out_degree': str(self.call_graph.get_out_degree(c)),
+                                               'descendant_entry_points_ratio': str(self.call_graph.get_descendants_exit_point_ratio(c)),
+                                               'descendant_exit_points_ratio': str(self.call_graph.get_ancestors_entry_point_ratio(c)),
+                                               'ancestor_entry_points_ratio': str(self.call_graph.get_ancestors_exit_point_ratio(c)),
+                                               'ancestor_exit_points_ratio': str(self.call_graph.get_ancestors_entry_point_ratio(c))
+                                           },
+                                           XElement('descendant_entry_points',
+                                                    {'count': str(len(self.call_graph.get_descendant_entry_points(c)))},
+                                                    [self.call_to_xml(c) for c in self.call_graph.get_descendant_entry_points(c)]),
+                                           XElement('descendant_exit_points',
+                                                    {'count': str(len(self.call_graph.get_descendant_exit_points(c)))},
+                                                    [self.call_to_xml(c) for c in self.call_graph.get_descendant_exit_points(c)]),
+                                           XElement('ancestor_entry_points',
+                                                    {'count': str(len(self.call_graph.get_ancestor_entry_points(c)))},
+                                                    [self.call_to_xml(c) for c in self.call_graph.get_ancestor_entry_points(c)]),
+                                           XElement('ancestor_exit_points',
+                                                    {'count': str(len(self.call_graph.get_ancestor_exit_points(c)))},
+                                                    [self.call_to_xml(c) for c in self.call_graph.get_ancestor_exit_points(c)]))
+                                  for c in self.call_graph.nodes]),
 
                         XElement("edges",
                                  {'count': str(len(self.call_graph.edges))},
@@ -43,57 +71,10 @@ class XmlFormatter(BaseFormatter):
                                   for xp in [[self.call_to_xml(c) for c in p]
                                              for p in self.call_graph.execution_paths]]),
 
-                        XElement('closeness', {},
-                                 [self.call_to_xml(k, closeness=str(v))
-                                  for k, v in self.call_graph.get_closeness().items()]),
-
-                        XElement('betweenness', {},
-                                 [self.call_to_xml(k, betweenness=str(v))
-                                  for k, v in self.call_graph.get_betweenness().items()]),
-
                         XElement('clustering',
                                  {'avg_entry_point_clustering': str(self.call_graph.entry_points_clustering),
-                                  'avg_exit_point_clustering': str(self.call_graph.exit_points_clustering)}),
-
-                        XElement('degree_centrality', {},
-                                 [self.call_to_xml(c,
-                                                   degree=str(self.call_graph.get_degree_centrality(c)),
-                                                   in_degree=str(self.call_graph.get_in_degree_centrality(c)),
-                                                   out_degree=str(self.call_graph.get_out_degree_centrality(c)))
-                                  for c in self.call_graph.nodes]),
-
-                        XElement('degree', {},
-                                 [self.call_to_xml(c,
-                                                   degree=str(self.call_graph.get_degree(c)),
-                                                   in_degree=str(self.call_graph.get_in_degree(c)),
-                                                   out_degree=str(self.call_graph.get_out_degree(c)))
-                                  for c in self.call_graph.nodes]),
-
-                        XElement('descendants', {},
-                                 [XElement('call',
-                                           {'function_name': c.function_name,
-                                            'descendant_entry_points_ratio': str(self.call_graph.get_descendants_exit_point_ratio(c)),
-                                            'descendant_exit_points_ratio': str(self.call_graph.get_ancestors_entry_point_ratio(c))},
-                                           XElement('descendant_entry_points',
-                                                    {'count': str(len(self.call_graph.get_descendant_entry_points(c)))},
-                                                    [self.call_to_xml(c) for c in self.call_graph.get_descendant_entry_points(c)]),
-                                           XElement('descendant_exit_points',
-                                                    {'count': str(len(self.call_graph.get_descendant_exit_points(c)))},
-                                                    [self.call_to_xml(c) for c in self.call_graph.get_descendant_exit_points(c)]))
-                                  for c in self.call_graph.nodes]),
-
-                        XElement('ancestors', {},
-                                 [XElement('call',
-                                           {'function_name': c.function_name,
-                                            'ancestor_entry_points_ratio': str(self.call_graph.get_ancestors_exit_point_ratio(c)),
-                                            'ancestor_exit_points_ratio': str(self.call_graph.get_ancestors_entry_point_ratio(c))},
-                                           XElement('ancestor_entry_points',
-                                                    {'count': str(len(self.call_graph.get_ancestor_entry_points(c)))},
-                                                    [self.call_to_xml(c) for c in self.call_graph.get_ancestor_entry_points(c)]),
-                                           XElement('ancestor_exit_points',
-                                                    {'count': str(len(self.call_graph.get_ancestor_exit_points(c)))},
-                                                    [self.call_to_xml(c) for c in self.call_graph.get_ancestor_exit_points(c)]))
-                                  for c in self.call_graph.nodes]))
+                                  'avg_exit_point_clustering': str(self.call_graph.exit_points_clustering)})
+        )
 
 
         print(self.prettyfy(root))
