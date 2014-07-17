c1d2a42fdd959f5ce55e623aa3fa0024247d16e0
Kevin Campusano <megakevinx@gmail.com>
2014-07-17 13:36:06 -0400

    Deleted obvious documentation of main method.

--- AttackSurfaceMeter/attack_surface_meter.py
+++ AttackSurfaceMeter/attack_surface_meter.py
@@ -7,9 +7,7 @@ from formatters import TxtFormatter, XmlFormatter, JsonFormatter, HtmlFormatter
 
 
 def main():
-    """
-        Execution entry point of the application.
-    """
+
     formatters = {
         'txt': TxtFormatter,
         'xml': XmlFormatter,
@@ -41,7 +39,6 @@ def parse_args():
     parser.add_argument("source_dir", help="Root directory of the source code to analyze.")
     parser.add_argument("-f", "--format", choices=["txt", "html", "xml", "json"], default="txt",
                         help="Output format of the calculated metrics.")
-    # parser.add_argument("-o", "--output", help="Output file.", default="output.metrics")
     parser.add_argument("-r", "--reverse", action="store_true",
                         help="When using cflow for call graph generation, use the reverse algorithm.")
 
