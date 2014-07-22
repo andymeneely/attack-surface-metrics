f3e0e80f4572c8f7aeea24d8a8104e8f92ff4428
Kevin Campusano <megakevinx@gmail.com>
2014-07-22 12:06:09 -0400

    Added new unit tests for txt and xml formatters and XElement.

--- AttackSurfaceMeter/tests/unit_tests.py
+++ AttackSurfaceMeter/tests/unit_tests.py
@@ -1,11 +1,9 @@
 __author__ = 'kevin'
 
 import unittest
-from attacksurfacemeter.stack import Stack
-from attacksurfacemeter.call import Call
-from attacksurfacemeter.call_graph import CallGraph
-
-from formatters.txt_formatter import TxtFormatter
+from attacksurfacemeter import Stack, Call, CallGraph
+from formatters import TxtFormatter, XmlFormatter
+from formatters.xml_formatter import XElement
 
 
 class StackTestCase(unittest.TestCase):
@@ -1396,6 +1394,175 @@ class TxtFormatterTestCase(unittest.TestCase):
 
         # Assert
         self.assertEqual(expected_value, degree)
+
+    def test_descendant_entry_points(self):
+        # Arrange
+        call = Call("main() <int main (void) at ./src/helloworld.c:58>:")
+        expected_count = 1
+        expected_content = [Call("greet_b() <void greet_b (int i) at ./src/helloworld.c:82>:")]
+
+        # Act
+        descendant_entry_points = self.formatter.get_descendant_entry_points(call)
+        all_found = all([c in descendant_entry_points for c in expected_content])
+
+        # [print(n.function_info) for n in self.call_graph.get_descendant_entry_points(call)]
+
+        # Assert
+        self.assertEqual(expected_count, len(descendant_entry_points))
+        self.assertTrue(all_found)
+
+    def test_descendant_exit_points(self):
+        # Arrange
+        call = Call("main() <int main (void) at ./src/helloworld.c:58>:")
+        expected_count = 5
+        expected_content = [Call("recursive_b() <void recursive_b (int i) at ./src/greetings.c:32> (R):"),
+                            Call("GreeterSayHiTo() <void GreeterSayHiTo (int value) at ./src/helloworld.c:53>:"),
+                            Call("greet() <void greet (int greeting_code) at ./src/greetings.c:14>:"),
+                            Call("recursive_a() <void recursive_a (int i) at ./src/greetings.c:26> (R):"),
+                            Call("GreeterSayHi() <void GreeterSayHi () at ./src/helloworld.c:48>:")]
+
+        # Act
+        descendant_exit_points = self.formatter.get_descendant_exit_points(call)
+        all_found = all([c in descendant_exit_points for c in expected_content])
+
+        # [print('Call("' + n.function_info + '"),') for n in self.call_graph.get_descendant_exit_points(call)]
+
+        # Assert
+        self.assertEqual(expected_count, len(descendant_exit_points))
+        self.assertTrue(all_found)
+
+    def test_descendants_entry_point_ratio(self):
+        # Arrange
+        call = Call("main() <int main (void) at ./src/helloworld.c:58>:")
+        expected_value = "0.07142857142857142"
+
+        # Act
+        ratio = self.formatter.get_descendants_entry_point_ratio(call)
+
+        # Assert
+        self.assertEqual(expected_value, ratio)
+
+    def test_descendants_exit_point_ratio(self):
+        # Arrange
+        call = Call("main() <int main (void) at ./src/helloworld.c:58>:")
+        expected_value = "0.35714285714285715"
+
+        # Act
+        ratio = self.formatter.get_descendants_exit_point_ratio(call)
+
+        # Assert
+        self.assertEqual(expected_value, ratio)
+
+    def test_ancestor_entry_points(self):
+        # Arrange
+        call = Call("greet() <void greet (int greeting_code) at ./src/greetings.c:14>:")
+        expected_count = 1
+        expected_content = [Call("greet_b() <void greet_b (int i) at ./src/helloworld.c:82>:")]
+
+        # Act
+        ancestor_entry_points = self.formatter.get_ancestor_entry_points(call)
+        all_found = all([c in ancestor_entry_points for c in expected_content])
+
+        # Assert
+        self.assertEqual(expected_count, len(ancestor_entry_points))
+        self.assertTrue(all_found)
+
+    def test_ancestor_exit_points(self):
+        # Arrange
+        call = Call("recursive_b() <void recursive_b (int i) at ./src/greetings.c:32> (R):")
+        expected_count = 2
+        expected_content = [Call("recursive_a() <void recursive_a (int i) at ./src/greetings.c:26> (R):"),
+                            Call("main() <int main (void) at ./src/helloworld.c:58>:")]
+
+        # Act
+        ancestor_exit_points = self.formatter.get_ancestor_exit_points(call)
+        all_found = all([c in ancestor_exit_points for c in expected_content])
+
+        # Assert
+        self.assertEqual(expected_count, len(ancestor_exit_points))
+        self.assertTrue(all_found)
+
+    def test_ancestors_entry_point_ratio(self):
+        # Arrange
+        call = Call("greet() <void greet (int greeting_code) at ./src/greetings.c:14>:")
+        expected_value = "0.3333333333333333"
+
+        # Act
+        ratio = self.formatter.get_ancestors_entry_point_ratio(call)
+
+        # Assert
+        self.assertEqual(expected_value, ratio)
+
+    def test_ancestors_exit_point_ratio(self):
+        # Arrange
+        call = Call("recursive_b() <void recursive_b (int i) at ./src/greetings.c:32> (R):")
+        expected_value = "0.5"
+
+        # Act
+        ratio = self.formatter.get_ancestors_exit_point_ratio(call)
+
+        # Assert
+        self.assertEqual(expected_value, ratio)
+
+
+class XmlFormatterTestCase(TxtFormatterTestCase):
+
+    def setUp(self):
+        self.formatter = XmlFormatter(CallGraph("./helloworld"))
+
+
+class XElementTestCase(unittest.TestCase):
+
+    def test_constructor(self):
+        # Arrange
+        tag = "element"
+
+        # Act
+        xelem = XElement(tag)
+
+        # Assert
+        self.assertEqual(tag, xelem.tag)
+
+    def test_constructor_with_attributes(self):
+        # Arrange
+        expected_attribs = {'att0': 0, 'att1': 1}
+
+        # Act
+        xelem = XElement('element', expected_attribs)
+
+        # Assert
+        self.assertEqual(expected_attribs, xelem.attrib)
+
+    def test_constructor_with_subelements_as_list(self):
+        # Arrange
+        expected_subelements = [XElement('subelem0'), XElement('subelem1')]
+
+        # Act
+        xelem = XElement('element',
+                         {'att0': 0, 'att1': 1},
+                         expected_subelements)
+
+        all_subelements_found = all([x in list(xelem) for x in expected_subelements])
+
+        # Assert
+        self.assertTrue(all_subelements_found)
+
+    def test_constructor_with_subelements_as_positional_arguments(self):
+        # Arrange
+        expected_subelement_1 = XElement('subelem0')
+        expected_subelement_2 = XElement('subelem1')
+
+        # Act
+        xelem = XElement('element',
+                         {'att0': 0, 'att1': 1},
+                         expected_subelement_1,
+                         expected_subelement_2)
+
+        all_subelements_found = all([expected_subelement_1 in list(xelem),
+                                     expected_subelement_2 in list(xelem)])
+
+        # Assert
+        self.assertTrue(all_subelements_found)
 
 
 if __name__ == '__main__':
