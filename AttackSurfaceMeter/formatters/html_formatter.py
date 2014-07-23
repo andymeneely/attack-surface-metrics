e2be4f590cabaacc0d5f8b91521bb0ecca3d6bec
Kevin Campusano <megakevinx@gmail.com>
2014-07-23 18:22:19 -0400

    Added all the calculated attack surface data to the context dictionary that is sent to the templating engine.

--- AttackSurfaceMeter/formatters/html_formatter.py
+++ AttackSurfaceMeter/formatters/html_formatter.py
@@ -31,7 +31,7 @@ class HtmlFormatter(BaseFormatter):
             'nodes_count': self.nodes_count,
             'nodes': [{
                           'function_name': c.function_name,
-                          'function_signature': '' if c.function_signature is None else c.function_signature,
+                          'function_signature': self._get_function_signature(c),
                           'closeness': self.get_closeness(c),
                           'betweenness': self.get_betweenness(c),
                           'degree_centrality': self.get_degree_centrality(c),
@@ -64,73 +64,36 @@ class HtmlFormatter(BaseFormatter):
                           'ancestor_exit_points': [{'function_name': aexp.function_name,
                                                     'function_signature': self._get_function_signature(aexp)
                                                    } for aexp in self.get_ancestor_exit_points(c)]
-                      } for c in self.nodes]
+                      } for c in self.nodes],
+
+            'edges_count': self.edges_count,
+            'edges': [{'from': f.function_name,
+                       'to': t.function_name
+                      } for (f, t) in self.edges],
+
+            'entry_points_count': self.entry_points_count,
+            'entry_points': [{'function_name': enp.function_name,
+                              'function_signature': self._get_function_signature(enp)
+                             } for enp in self.entry_points],
+
+            'exit_points_count': self.exit_points_count,
+            'exit_points': [{'function_name': exp.function_name,
+                             'function_signature': self._get_function_signature(exp)
+                             } for exp in self.exit_points],
+
+            'entry_points_clustering': self.entry_points_clustering,
+            'exit_points_clustering': self.exit_points_clustering,
+
+            'execution_paths_count': self.execution_paths_count,
+            'execution_paths_average': self.average_execution_path_length,
+            'execution_paths_median': self.median_execution_path_length,
+
+            'execution_paths': [{'length': str(len(xp)),
+                                 'path': [{'function_name': cxp.function_name,
+                                           'function_signature': self._get_function_signature(cxp)}
+                                          for cxp in xp]}
+                                for xp in self.execution_paths]
         })
-
-
-
-
-        # root = XElement("attack_surface",
-        #                 {'directory': self.source_dir},
-        #                 XElement("nodes",
-        #                          {'count': self.nodes_count},
-        #                          [self.call_to_xml(c,
-        #                                            {
-        #                                                'closeness': self.get_closeness(c),
-        #                                                'betweenness': self.get_betweenness(c),
-        #                                                'degree_centrality': self.get_degree_centrality(c),
-        #                                                'in_degree_centrality': self.get_in_degree_centrality(c),
-        #                                                'out_degree_centrality': self.get_out_degree_centrality(c),
-        #                                                'degree': self.get_degree(c),
-        #                                                'in_degree': self.get_in_degree(c),
-        #                                                'out_degree': self.get_out_degree(c),
-        #                                                'descendant_entry_points_ratio': self.get_descendants_entry_point_ratio(c),
-        #                                                'descendant_exit_points_ratio': self.get_descendants_exit_point_ratio(c),
-        #                                                'ancestor_entry_points_ratio': self.get_ancestors_entry_point_ratio(c),
-        #                                                'ancestor_exit_points_ratio': self.get_ancestors_exit_point_ratio(c)
-        #                                            },
-        #                                            XElement('descendant_entry_points',
-        #                                                     {'count': self.get_descendant_entry_points_count(c)},
-        #                                                     [self.call_to_xml(c) for c in self.get_descendant_entry_points(c)]),
-        #                                            XElement('descendant_exit_points',
-        #                                                     {'count': self.get_descendant_exit_points_count(c)},
-        #                                                     [self.call_to_xml(c) for c in self.get_descendant_exit_points(c)]),
-        #                                            XElement('ancestor_entry_points',
-        #                                                     {'count': self.get_ancestor_entry_points_count(c)},
-        #                                                     [self.call_to_xml(c) for c in self.get_ancestor_entry_points(c)]),
-        #                                            XElement('ancestor_exit_points',
-        #                                                     {'count': self.get_ancestor_exit_points_count(c)},
-        #                                                     [self.call_to_xml(c) for c in self.get_ancestor_exit_points(c)]))
-        #                           for c in self.nodes]),
-        #
-        #                 XElement("edges",
-        #                          {'count': self.edges_count},
-        #                          [XElement('edge',
-        #                                    {'from': f.function_name, 'to': t.function_name})
-        #                           for (f, t) in self.edges]),
-        #
-        #                 XElement('entry_points',
-        #                          {'count': self.entry_points_count},
-        #                          [self.call_to_xml(c) for c in self.entry_points]),
-        #
-        #                 XElement('exit_points',
-        #                          {'count': self.exit_points_count},
-        #                          [self.call_to_xml(c) for c in self.exit_points]),
-        #
-        #                 XElement('execution_paths',
-        #                          {
-        #                              'count': self.execution_paths_count,
-        #                              'average': self.average_execution_path_length,
-        #                              'median': self.median_execution_path_length
-        #                          },
-        #                          [XElement('path', {'length': str(len(xp))}, xp)
-        #                           for xp in [[self.call_to_xml(c) for c in p]
-        #                                      for p in self.execution_paths]]),
-        #
-        #                 XElement('clustering',
-        #                          {'entry_points_clustering': self.entry_points_clustering,
-        #                           'exit_points_clustering': self.exit_points_clustering})
-        # )
 
         output = template.render(context)
 
