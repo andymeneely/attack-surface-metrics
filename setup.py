797d44247f92a98acfacb2bf027cb58fb30bb0af
Kevin Campusano <megakevinx@gmail.com>
2014-08-01 15:49:12 -0400

    Updated setup.py and MANIFEST to work with new files and directory structure.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name='AttackSurfaceMeter',
-    version='0.0.1',
-    packages=['attacksurfacemeter',],
+    version='0.0.2',
+    packages=['attacksurfacemeter', 'formatters'],
     requires=['networkx', 'matplotlib'],
     license='The MIT License (MIT) Copyright (c) 2014 Andy Meneely',
     description='Library for collecting metrics of the attack surface.',
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
     author='Kevin Campusano',
     author_email='kac2375@rit.edu',
     url='https://github.com/andymeneely/attack-surface-metrics'
