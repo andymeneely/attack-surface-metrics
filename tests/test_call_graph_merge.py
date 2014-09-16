06bf551bbc18d5bd3060b5ac86fa3e6a2953ecf8
Kevin Campusano <megakevinx@gmail.com>
2014-09-16 13:35:33 -0400

    Added unit test for edges in merged call graph.

--- tests/test_call_graph_merge.py
+++ tests/test_call_graph_merge.py
@@ -56,6 +56,45 @@ class CallGraphFileTestCase(unittest.TestCase):
         self.assertEqual(15, len(nodes))
         self.assertTrue(all_calls_found)
 
+    def test_edges(self):
+        # Arrange
+        expected_content = [(Call("recursive_a", ""), Call("printf", "")),
+                            (Call("recursive_a", ""), Call("recursive_b", "")),
+                            (Call("new_Greeter", "helloworld.c"), Call("GreeterSayHi", "helloworld.c")),
+                            (Call("new_Greeter", "helloworld.c"), Call("malloc", "")),
+                            (Call("new_Greeter", "helloworld.c"), Call("GreeterSayHiTo", "")),
+                            (Call("recursive_b", ""), Call("printf", "")),
+                            (Call("recursive_b", ""), Call("recursive_a", "")),
+                            (Call("main", "helloworld.c"), Call("greet_a", "helloworld.c")),
+                            (Call("main", "helloworld.c"), Call("GreeterSayHi", "helloworld.c")),
+                            (Call("main", "helloworld.c"), Call("functionPtr", "helloworld.c")),
+                            (Call("main", "helloworld.c"), Call("greet_b", "helloworld.c")),
+                            (Call("main", "helloworld.c"), Call("puts", "")),
+                            (Call("main", "helloworld.c"), Call("printf", "")),
+                            (Call("main", "helloworld.c"), Call("new_Greeter", "helloworld.c")),
+                            (Call("main", "helloworld.c"), Call("GreeterSayHiTo", "")),
+                            (Call("main", "helloworld.c"), Call("addInt", "helloworld.c")),
+                            (Call("GreeterSayHiTo", ""), Call("printf", "")),
+                            (Call("greet", ""), Call("puts", "")),
+                            (Call("greet_a", "helloworld.c"), Call("recursive_a", "")),
+                            (Call("greet_a", "helloworld.c"), Call("greet", "")),
+                            (Call("GreeterSayHi", "helloworld.c"), Call("printf", "")),
+                            (Call("greet_b", "helloworld.c"), Call("recursive_b", "")),
+                            (Call("greet_b", "helloworld.c"), Call("gets", "")),
+                            (Call("greet_b", "helloworld.c"), Call("greet", ""))]
+
+        # Act
+        edges = self.call_graph.edges
+        all_calls_found = all([c in edges for c in expected_content])
+
+        # for e in edges:
+        #     print('(Call("' + e[0].function_name + '", "' + (e[0].function_signature if e[0].function_signature else "") + '"), ' +
+        #            'Call("' + e[1].function_name + '", "' + (e[1].function_signature if e[1].function_signature else "") + '")),')
+
+        # Assert
+        self.assertEqual(24, len(edges))
+        self.assertTrue(all_calls_found)
+
 
 if __name__ == '__main__':
     unittest.main()
