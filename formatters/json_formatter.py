b2f9cee8cff77638ab95d07e32b77c8843668da1
Kevin Campusano <megakevinx@gmail.com>
2014-09-27 01:08:43 -0400

    Wrote code for using template files for JsonFormatter. This still doesn't work because the template hasn't been written.

--- formatters/json_formatter.py
+++ formatters/json_formatter.py
@@ -4,5 +4,17 @@ from formatters.base_formatter import BaseFormatter
 
 
 class JsonFormatter(BaseFormatter):
+    """
+        Produces a json report with the attack surface metrics calculated by call_graph
+        and prints it to console.
+    """
     def __init__(self, call_graph):
         super(JsonFormatter, self).__init__(call_graph)
+
+    @property
+    def template_file(self):
+        return "template.json"
+
+    @property
+    def summary_template_file(self):
+        return "summary_template.json"
