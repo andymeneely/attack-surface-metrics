ed9faa323bc0711138f47b7da68c95447428ac86
Kevin Campusano <megakevinx@gmail.com>
2014-09-27 01:12:57 -0400

    Fixed unit tests.

--- tests/test_call_graph_merge.py
+++ tests/test_call_graph_merge.py
@@ -58,30 +58,28 @@ class CallGraphMergeTestCase(unittest.TestCase):
 
     def test_edges(self):
         # Arrange
-        expected_content = [(Call("recursive_a", ""), Call("printf", "")),
-                            (Call("recursive_a", ""), Call("recursive_b", "")),
-                            (Call("new_Greeter", "helloworld.c"), Call("GreeterSayHi", "helloworld.c")),
-                            (Call("new_Greeter", "helloworld.c"), Call("malloc", "")),
-                            (Call("new_Greeter", "helloworld.c"), Call("GreeterSayHiTo", "")),
-                            (Call("recursive_b", ""), Call("printf", "")),
-                            (Call("recursive_b", ""), Call("recursive_a", "")),
-                            (Call("main", "helloworld.c"), Call("greet_a", "helloworld.c")),
-                            (Call("main", "helloworld.c"), Call("GreeterSayHi", "helloworld.c")),
-                            (Call("main", "helloworld.c"), Call("functionPtr", "helloworld.c")),
-                            (Call("main", "helloworld.c"), Call("greet_b", "helloworld.c")),
-                            (Call("main", "helloworld.c"), Call("puts", "")),
-                            (Call("main", "helloworld.c"), Call("printf", "")),
-                            (Call("main", "helloworld.c"), Call("new_Greeter", "helloworld.c")),
-                            (Call("main", "helloworld.c"), Call("GreeterSayHiTo", "")),
-                            (Call("main", "helloworld.c"), Call("addInt", "helloworld.c")),
+        expected_content = [(Call("new_Greeter", ""), Call("malloc", "")),
+                            (Call("greet_b", ""), Call("gets", "")),
+                            (Call("greet_b", ""), Call("recursive_b", "")),
+                            (Call("greet_b", ""), Call("greet", "")),
+                            (Call("GreeterSayHi", ""), Call("printf", "")),
                             (Call("GreeterSayHiTo", ""), Call("printf", "")),
                             (Call("greet", ""), Call("puts", "")),
-                            (Call("greet_a", "helloworld.c"), Call("recursive_a", "")),
-                            (Call("greet_a", "helloworld.c"), Call("greet", "")),
-                            (Call("GreeterSayHi", "helloworld.c"), Call("printf", "")),
-                            (Call("greet_b", "helloworld.c"), Call("recursive_b", "")),
-                            (Call("greet_b", "helloworld.c"), Call("gets", "")),
-                            (Call("greet_b", "helloworld.c"), Call("greet", ""))]
+                            (Call("recursive_a", ""), Call("printf", "")),
+                            (Call("recursive_a", ""), Call("recursive_b", "")),
+                            (Call("recursive_b", ""), Call("printf", "")),
+                            (Call("recursive_b", ""), Call("recursive_a", "")),
+                            (Call("main", ""), Call("new_Greeter", "")),
+                            (Call("main", ""), Call("puts", "")),
+                            (Call("main", ""), Call("GreeterSayHiTo", "")),
+                            (Call("main", ""), Call("greet_b", "")),
+                            (Call("main", ""), Call("functionPtr", "helloworld.c")),
+                            (Call("main", ""), Call("GreeterSayHi", "")),
+                            (Call("main", ""), Call("printf", "")),
+                            (Call("main", ""), Call("greet_a", "")),
+                            (Call("main", ""), Call("addInt", "")),
+                            (Call("greet_a", ""), Call("recursive_a", "")),
+                            (Call("greet_a", ""), Call("greet", ""))]
 
         # Act
         edges = self.call_graph.edges
@@ -92,7 +90,7 @@ class CallGraphMergeTestCase(unittest.TestCase):
         #            'Call("' + e[1].function_name + '", "' + (e[1].function_signature if e[1].function_signature else "") + '")),')
 
         # Assert
-        self.assertEqual(24, len(edges))
+        self.assertEqual(22, len(edges))
         self.assertTrue(all_calls_found)
 
 
