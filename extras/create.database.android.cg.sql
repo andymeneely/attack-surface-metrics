ae0e8ea0cdb9a493e6587b9ad14734333425c49a
Kevin Campusano <megakevinx@gmail.com>
2015-03-01 21:14:57 -0500

    Added a script to load a sqlite database with the information of all the edges in an android app's call graph

---
+++ extras/create.database.android.cg.sql
@@ -0,0 +1,19 @@
+--CREATE TABLE "nodes" (
+--    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+--    "call" TEXT NOT NULL
+--)
+--
+--CREATE TABLE "edges" (
+--    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+--    "callee" INTEGER NOT NULL,
+--    "caller" INTEGER NOT NULL
+--);
+--
+--CREATE UNIQUE INDEX "uk_call" on nodes (call ASC)
+
+-- Describe EDGES
+CREATE TABLE "edges" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "callee" TEXT NOT NULL,
+    "caller" TEXT NOT NULL
+)
