1aea8aca7728fc946433ce29344f85f01fd0fa26
Kevin Campusano <megakevinx@gmail.com>
2015-04-22 13:37:18 -0400

    Minor stylistic cahnge.

--- attacksurfacemeter/android_call_graph.py
+++ attacksurfacemeter/android_call_graph.py
@@ -103,9 +103,9 @@ class AndroidCallGraph(CallGraph):
 
     @staticmethod
     def _merge_dicts(x, y):
-        '''
+        """
             Given two dicts, merge them into a new dict as a shallow copy.
-        '''
+        """
         z = x.copy()
         z.update(y)
         return z
