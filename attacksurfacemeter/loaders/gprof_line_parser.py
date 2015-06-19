b84c2eb7542e4ac160fe02bbe0f88b470a111fef
Nuthan Munaiah <nuthan.munaiah@live.com>
2015-06-18 20:54:02 -0400

    gprof line parsing now uses fewer regexes

--- attacksurfacemeter/loaders/gprof_line_parser.py
+++ attacksurfacemeter/loaders/gprof_line_parser.py
@@ -1,8 +1,9 @@
-__author__ = 'kevin'
-
 import re
 
 from attacksurfacemeter.loaders.base_line_parser import BaseLineParser
+
+# Regular expression to parse the name field from gprof call graph
+RE_NAME = re.compile('^(\w+)(?:(?:\(?.*\)?\s\()([\w\.\-\/]+\.\w+))?')
 
 
 class GprofLineParser(BaseLineParser):
@@ -24,48 +25,18 @@ class GprofLineParser(BaseLineParser):
     def load(self, gprof_line):
         self.__init__()
 
-        match = re.search(r"(\[\d+\])( +)((\d+\.\d+)( +)){3}(\d*\+*\d*)( +)([\w.]+)( +)(.*)", gprof_line)
-
-        if not match:
-            match = re.search(r"(\[\d+)( +)((\d+\.\d+)( +)){3}([\d\+]*)( +)([\w.]+)( +)(.*)", gprof_line)
-
+        # The field in the gprof call graph that contains the name of the
+        # function and the file containing the function begins at column 46 
+        # (deduced from gprof source code)
+        name = gprof_line[45:]
+        match = RE_NAME.match(name.strip())
+        
         if match:
-            self._function_name = match.group(8)
-            self._function_signature = GprofLineParser._get_file_name(match.group(10))
+            groups = match.groups(default='')
+            self._function_name = groups[0]
+            self._function_signature = groups[1]
         else:
-            match = re.search(r"( +)((\d+\.\d+)( +)){2}(\d+/\d+)( +)([\w.]+)( +)(.*)", gprof_line)
+            raise ValueError(
+                'Unable to parse gprof line - "{0}"'.format(gprof_line)
+            )
 
-            if match:
-                self._function_name = match.group(7)
-                self._function_signature = GprofLineParser._get_file_name(match.group(9))
-            else:
-                match = re.search(r"( +)(\d+)( +)(\w+)( +)(.*)", gprof_line)
-
-                if match:
-                    try:
-                        self._function_name = match.group(4)
-                        self._function_signature = GprofLineParser._get_file_name(match.group(6))
-                    except:
-                        raise ValueError("Can not parse a call from the received text.")
-                else:
-                    raise ValueError("Can not parse a call from the received text.")
-
-    @staticmethod
-    def _get_file_name(text_fragment):
-        """
-            Obtains the name of the file where the function represented is defined.
-
-            Args:
-               text_fragment: A String containing a fragment from a line of gprof's output.
-
-            Returns:
-                A String containing the name of the file where the function detailed in the fragment is defined.
-        """
-        regex = r"([-.\w\/]+\.(?:c|asm|h))"
-
-        match = re.search(regex, text_fragment)
-
-        if match:
-            return re.search(regex, text_fragment).group(0)
-        else:
-            return ""
