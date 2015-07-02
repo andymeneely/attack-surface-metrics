80dec2a9e69857ab262e8fac40302ea077d03c81
Nuthan Munaiah <nuthan.munaiah@live.com>
2015-07-02 01:08:45 -0400

    [REFACTOR] Utilities moved to its own module

---
+++ attacksurfacemeter/utilities.py
@@ -0,0 +1,181 @@
+import copy
+import networkx as nx
+
+from attacksurfacemeter.call import Call
+
+
+def fix(call_graph, using):
+    """Fix a call graph using information from another, reference call graph.
+
+    Function attempts to a fix a call graph using a reference call graph
+    which is assumed to be more comprehensive than the one being fixed.
+    The notion of fixing a call graph entails identifying a node without
+    function_signature and replacing it with an identical node from the
+    reference call graph that has a function_signature.
+
+    A call graph is required to be fixed to ensure compatibility of
+    equivalent nodes so that later when merging the two call graphs' sets
+    of edges, networkx doesn't think the cflow and gprof calls are
+    different only because one of them doesn't have a function_signature
+    associated to it.
+
+    Parameters
+    ----------
+    call_graph : CallGraph
+        An instance of CallGraph that represents a call graph to be fixed.
+    using : CallGraph
+        An instance of CallGraph that represents a call graph assumed to
+        be more comprehensive than call graph required to be fixed. In other
+        words, the using call graph is a reference call graph.
+
+    Notes
+    -----
+
+    networkx internally uses a dictionary as the data structure to store
+    nodes. Modifying an attribute that is used in the computation of the
+    object's hash will invalidate the hash leaving the graph in an
+    inconsistent state. Hence, fixing nodes involves replacing existing
+    nodes with their fixed equivalents.
+
+    More details at http://networkx.lanl.gov/tutorial/tutorial.html
+    """
+    nodes_to_replace = []
+
+    for node in [n for (n, _) in call_graph.nodes if not n.function_signature]:
+        reference_nodes = [
+            n for (n, _) in using.nodes
+            if n.function_name == node.function_name
+        ]
+
+        if len(reference_nodes) == 1:
+            new_node = Call(
+                node.function_name,
+                reference_nodes[0].function_signature,
+                node.environment
+            )
+            nodes_to_replace.append((node, new_node))
+
+    for (before, after) in nodes_to_replace:
+        call_graph.call_graph.add_node(
+            after,
+            call_graph.call_graph.node[before]
+        )
+
+        # Edges terminating at the node to be replaced
+        for predecessor in call_graph.call_graph.predecessors(before):
+            call_graph.call_graph.add_edge(
+                predecessor, after,
+                call_graph.call_graph.get_edge_data(
+                    predecessor, before
+                )
+            )
+
+        # Edges originating at the node to be replaced
+        for successor in call_graph.call_graph.successors(before):
+            call_graph.call_graph.add_edge(
+                after, successor,
+                call_graph.call_graph.get_edge_data(
+                    before, successor
+                )
+            )
+
+        call_graph.call_graph.remove_node(before)
+
+
+def get_fragments(graph):
+    """Return a list of strongly connected components of a graph.
+
+    Parameters
+    ----------
+    graph : NetworkX DiGraph
+        A strongly connected instance of NetworkX DiGraph object.
+
+    Returns
+    -------
+    fragments : list
+        A list of strongly connected instances of NetworkX DiGraph each of
+        which represents a component in the graph.
+    """
+    if not isinstance(graph, nx.DiGraph):
+        raise Exception(
+            'get_fragments operation not defined for undirected graphs.'
+        )
+
+    return list(nx.strongly_connected_component_subgraphs(graph))
+
+
+def get_largest_fragment(fragments):
+    """Return the largest connected component in a list of components.
+
+    Parameters
+    ----------
+    fragments : list
+        A list of strongly connected instances of NetworkX DiGraph each of
+        which represents a component in the graph.
+
+    Returns
+    -------
+    fragment : NetworkX DiGraph
+        A strongly connected instance of NetworkX DiGraph that represents the
+        largest component in the graph.
+    """
+    fragments.sort(key=lambda f: len(f.nodes()), reverse=True)
+    return fragments[0]
+
+
+def get_node_attrs(source, caller, callee, defenses, vulnerabilities):
+    """Return node attributes.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    attributes : tuple
+        A tuple of two dictionary elements: caller_attrs and callee_attrs. The
+        callee_attrs may be None if the callee is a standard library function.
+        Each dictionary may contain the following keys:
+
+        Common Keys:
+
+        tested: True if the node is tested, False otherwise.
+        defense: Set if the node represents a function which is a designed
+            defense in the system.
+        vulnerable: Set if the node represents a vulnerable function in the
+            system.
+
+        Caller-specific Keys:
+
+        dangerous: Set if the callee is a dangerous system function.
+        entry: Set if the callee is an standard input function.
+        exit: Set if the callee is a standard output function.
+    """
+    caller_attrs = dict()
+    callee_attrs = None
+
+    if caller in defenses:
+        caller_attrs['defense'] = None
+    if caller in vulnerabilities:
+        caller_attrs['vulnerable'] = None
+    if callee is not None:
+        if 'gprof' in source:
+            caller_attrs['tested'] = None
+        if callee.in_stdlib():
+            if callee.is_dangerous():
+                caller_attrs['dangerous'] = None
+            if callee.is_input():
+                caller_attrs['entry'] = None
+            if callee.is_output():
+                caller_attrs['exit'] = None
+        else:
+            callee_attrs = dict()
+            if 'gprof' in source:
+                callee_attrs['tested'] = None
+            if callee in defenses:
+                callee_attrs['defense'] = None
+            if callee in vulnerabilities:
+                callee_attrs['vulnerable'] = None
+
+    attributes = (caller_attrs, callee_attrs)
+
+    return attributes
---
+++ tests/test_utilities.py
@@ -0,0 +1,457 @@
+import copy
+import os
+import unittest
+
+import networkx as nx
+
+from attacksurfacemeter import utilities
+from attacksurfacemeter.call import Call
+from attacksurfacemeter.call_graph import CallGraph
+from attacksurfacemeter.environments import Environments
+from attacksurfacemeter.loaders.cflow_loader import CflowLoader
+from attacksurfacemeter.loaders.gprof_loader import GprofLoader
+
+
+class UtilitiesTestCase(unittest.TestCase):
+    def test_fix(self):
+        # Arrange
+        target = CallGraph.from_loader(
+            CflowLoader(
+                os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)),
+                    'helloworld/cflow.callgraph.r.mod.txt'
+                ),
+                True
+            )
+        )
+        _target = copy.deepcopy(target)
+        reference = CallGraph.from_loader(
+            GprofLoader(
+                os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)),
+                    'helloworld/gprof.callgraph.txt'
+                )
+            )
+        )
+        expected = {
+            'before': Call('GreeterSayHi', '', Environments.C),
+            'after': Call('GreeterSayHi', './src/helloworld.c', Environments.C)
+        }
+
+        # Act
+        utilities.fix(target, using=reference)
+        actual = {
+            'before':  next(
+                i
+                for (i, _) in _target.nodes
+                if i.function_name == 'GreeterSayHi'
+            ),
+            'after':  next(
+                i
+                for (i, _) in target.nodes
+                if i.function_name == 'GreeterSayHi'
+            )
+        }
+
+        # Assert
+        self.assertEqual(expected['before'], actual['before'])
+        self.assertEqual(expected['after'], actual['after'])
+        # Asserting if node attributes got carried over
+        self.assertCountEqual(
+            [
+                attrs
+                for (i, attrs) in _target.nodes
+                if i == expected['before']
+            ],
+            [
+                attrs
+                for (i, attrs) in target.nodes
+                if i == expected['after']
+            ]
+        )
+        # Asserting if edge attributes got carried over
+        self.assertCountEqual(
+            [
+                attrs
+                for (i, j, attrs) in _target.edges
+                if i == expected['before'] or j == expected['before']
+            ],
+            [
+                attrs
+                for (i, j, attrs) in target.edges
+                if i == expected['after'] or j == expected['after']
+            ],
+        )
+        # Asserting if OTHER nodes and their attributes got carried over
+        self.assertCountEqual(
+            [
+                (i, attrs)
+                for (i, attrs) in _target.nodes
+                if i != expected['before']
+            ],
+            [
+                (i, attrs)
+                for (i, attrs) in target.nodes
+                if i != expected['after']
+            ]
+        )
+        # Asserting if OTHER edges and their attributes got carried over
+        self.assertCountEqual(
+            [
+                (i, j, attrs)
+                for (i, j, attrs) in _target.edges
+                if i != expected['before'] and j != expected['before']
+            ],
+            [
+                (i, j, attrs)
+                for (i, j, attrs) in target.edges
+                if i != expected['after'] and j != expected['after']
+            ],
+        )
+
+    def test_get_fragments(self):
+        # Arrange
+
+        #   a -- b   e -- f -- g
+        #   |    |
+        #   |    |
+        #   d -- c   h -- i  j
+        graph = nx.DiGraph()
+        graph.add_nodes_from(
+            ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+        )
+        graph.add_edges_from([
+           ('a', 'b'), ('b', 'a'), ('b', 'c'), ('c', 'b'), ('c', 'd'),
+           ('d', 'c'), ('d', 'a'), ('a', 'd'), ('e', 'f'), ('f', 'e'),
+           ('f', 'g'), ('g', 'f'), ('h', 'i'), ('i', 'h')
+        ])
+
+        expected = [None] * 4
+        expected[0] = nx.DiGraph()
+        expected[0].add_nodes_from(['a', 'b', 'c', 'd'])
+        expected[0].add_edges_from([
+           ('a', 'b'), ('b', 'a'), ('b', 'c'), ('c', 'b'), ('c', 'd'),
+           ('d', 'c'), ('d', 'a'), ('a', 'd')
+        ])
+
+        expected[1] = nx.DiGraph()
+        expected[1].add_nodes_from(['e', 'f', 'g'])
+        expected[1].add_edges_from(
+            [('e', 'f'), ('f', 'e'), ('f', 'g'), ('g', 'f')]
+        )
+
+        expected[2] = nx.DiGraph()
+        expected[2].add_nodes_from(['h', 'i'])
+        expected[2].add_edges_from([('i', 'h'), ('h', 'i')])
+        
+        expected[3] = nx.DiGraph()
+        expected[3].add_nodes_from(['j'])
+
+        # Act
+        actual = utilities.get_fragments(graph)
+        actual.sort(key=lambda i: len(i.nodes()), reverse=True)
+
+        # Assert
+        self.assertEqual(len(expected), len(actual))
+        for i in range(4):
+            self.assertCountEqual(expected[i].nodes(), actual[i].nodes())
+            self.assertCountEqual(expected[i].edges(), actual[i].edges())
+
+    def test_get_fragments_for_undirected(self):
+        # Arrange
+
+        #   a -- b   e -- f -- g
+        #   |    |
+        #   |    |
+        #   d -- c   h -- i  j
+        graph = nx.Graph()
+        graph.add_nodes_from(
+            ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+        )
+        graph.add_edges_from([
+           ('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a'), ('e', 'f'),
+           ('f', 'g'), ('h', 'i')
+        ])
+
+        # Assert
+        self.assertRaises(Exception, utilities.get_fragments, graph)
+
+    def test_get_largest_fragment(self):
+        # Arrange
+
+        #   a -- b   e -- f -- g
+        #   |    |
+        #   |    |
+        #   d -- c   h -- i  j
+        graph = nx.DiGraph()
+        graph.add_nodes_from(
+            ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+        )
+        graph.add_edges_from([
+           ('a', 'b'), ('b', 'a'), ('b', 'c'), ('c', 'b'), ('c', 'd'),
+           ('d', 'c'), ('d', 'a'), ('a', 'd'), ('e', 'f'), ('f', 'e'),
+           ('f', 'g'), ('g', 'f'), ('h', 'i'), ('i', 'h')
+        ])
+
+        expected = nx.DiGraph()
+        expected.add_nodes_from(['a', 'b', 'c', 'd'])
+        expected.add_edges_from([
+           ('a', 'b'), ('b', 'a'), ('b', 'c'), ('c', 'b'), ('c', 'd'),
+           ('d', 'c'), ('d', 'a'), ('a', 'd')
+        ])
+
+        # Act
+        actual = utilities.get_largest_fragment(utilities.get_fragments(graph))
+
+        # Assert
+        self.assertCountEqual(expected.nodes(), actual.nodes())
+        self.assertCountEqual(expected.edges(), actual.edges())
+
+    def test_get_node_attrs(self):
+        # Scenario: main -- printf (cflow)
+
+        # Arrange
+        source = 'cflow'
+        caller = Call('main', 'main.c', Environments.C)
+        callee = Call('printf', '', Environments.C)
+
+        # Act
+        (caller_attrs, callee_attrs) = utilities.get_node_attrs(
+            source, caller, callee, list(), list()
+        )
+
+        # Assert
+
+        # Caller Attributes
+        self.assertTrue('tested' not in caller_attrs)
+        self.assertTrue('defense' not in caller_attrs)
+        self.assertTrue('vulnerable' not in caller_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' in caller_attrs)
+
+        # Callee Attributes
+        self.assertIsNone(callee_attrs)
+
+        # Scenario: main -- printf (gprof)
+
+        # Arrange
+        source = 'gprof'
+        caller = Call('main', 'main.c', Environments.C)
+        callee = Call('printf', '', Environments.C)
+
+        # Act
+        (caller_attrs, callee_attrs) = utilities.get_node_attrs(
+            source, caller, callee, list(), list()
+        )
+
+        # Assert
+
+        # Caller Attributes
+        self.assertTrue('tested' in caller_attrs)
+        self.assertTrue('defense' not in caller_attrs)
+        self.assertTrue('vulnerable' not in caller_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' in caller_attrs)
+
+        # Callee Attributes
+        self.assertIsNone(callee_attrs)
+
+        # Scenario: main -- None (gprof)
+
+        # Arrange
+        source = 'gprof'
+        caller = Call('main', 'main.c', Environments.C)
+        callee = None
+
+        # Act
+        (caller_attrs, callee_attrs) = utilities.get_node_attrs(
+            source, caller, callee, list(), list()
+        )
+
+        # Assert
+
+        # Caller Attributes
+        self.assertTrue('tested' not in caller_attrs)
+        self.assertTrue('defense' not in caller_attrs)
+        self.assertTrue('vulnerable' not in caller_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' not in caller_attrs)
+
+        # Callee Attributes
+        self.assertIsNone(callee_attrs)
+
+        # Scenario: main -- validate* (cflow)
+        #   * Designed defense
+
+        # Arrange
+        source = 'cflow'
+        defenses = [Call('validate', 'utils.c', Environments.C)]
+        caller = Call('main', 'main.c', Environments.C)
+        callee = Call('validate', 'utils.c', Environments.C)
+
+        # Act
+        (caller_attrs, callee_attrs) = utilities.get_node_attrs(
+            source, caller, callee, defenses, list()
+        )
+
+        # Assert
+
+        # Caller Attributes
+        self.assertTrue('tested' not in caller_attrs)
+        self.assertTrue('defense' not in caller_attrs)
+        self.assertTrue('vulnerable' not in caller_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' not in caller_attrs)
+
+        # Callee Attributes
+        self.assertIsNotNone(callee_attrs)
+        self.assertTrue('tested' not in callee_attrs)
+        self.assertTrue('defense' in callee_attrs)
+        self.assertTrue('vulnerable' not in caller_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' not in caller_attrs)
+
+        # Scenario: main -- validate* (cflow)
+        #   * Vulnerable
+
+        # Arrange
+        source = 'cflow'
+        vulnerabilities = [Call('validate', 'utils.c', Environments.C)]
+        caller = Call('main', 'main.c', Environments.C)
+        callee = Call('validate', 'utils.c', Environments.C)
+
+        # Act
+        (caller_attrs, callee_attrs) = utilities.get_node_attrs(
+            source, caller, callee, list(), vulnerabilities
+        )
+
+        # Assert
+
+        # Caller Attributes
+        self.assertTrue('tested' not in caller_attrs)
+        self.assertTrue('defense' not in callee_attrs)
+        self.assertTrue('vulnerable' not in caller_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' not in caller_attrs)
+
+        # Callee Attributes
+        self.assertIsNotNone(callee_attrs)
+        self.assertTrue('tested' not in callee_attrs)
+        self.assertTrue('defense' not in callee_attrs)
+        self.assertTrue('vulnerable' in callee_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' not in caller_attrs)
+
+        # Scenario: main* -- validate+ (cflow)
+        #   * Vulnerable
+        #   + Designed defense and vulnerable
+
+        # Arrange
+        source = 'cflow'
+        defenses = [Call('validate', 'utils.c', Environments.C)]
+        vulnerabilities = [
+            Call('main', 'main.c', Environments.C),
+            Call('validate', 'utils.c', Environments.C)
+        ]
+        caller = Call('main', 'main.c', Environments.C)
+        callee = Call('validate', 'utils.c', Environments.C)
+
+        # Act
+        (caller_attrs, callee_attrs) = utilities.get_node_attrs(
+            source, caller, callee, defenses, vulnerabilities
+        )
+
+        # Assert
+
+        # Caller Attributes
+        self.assertTrue('tested' not in caller_attrs)
+        self.assertTrue('defense' not in caller_attrs)
+        self.assertTrue('vulnerable' in caller_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' not in caller_attrs)
+
+        # Callee Attributes
+        self.assertIsNotNone(callee_attrs)
+        self.assertTrue('tested' not in callee_attrs)
+        self.assertTrue('defense' in callee_attrs)
+        self.assertTrue('vulnerable' in callee_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' not in caller_attrs)
+
+        # Scenario: main* -- validate+ (cflow)
+        #   * Designed defense
+        #   + Designed defense and vulnerable
+
+        # Arrange
+        source = 'cflow'
+        defenses = [
+            Call('main', 'main.c', Environments.C),
+            Call('validate', 'utils.c', Environments.C)
+        ]
+        vulnerabilities = [
+            Call('main', 'main.c', Environments.C),
+            Call('validate', 'utils.c', Environments.C)
+        ]
+        caller = Call('main', 'main.c', Environments.C)
+        callee = Call('validate', 'utils.c', Environments.C)
+
+        # Act
+        (caller_attrs, callee_attrs) = utilities.get_node_attrs(
+            source, caller, callee, defenses, vulnerabilities
+        )
+
+        # Assert
+
+        # Caller Attributes
+        self.assertTrue('tested' not in caller_attrs)
+        self.assertTrue('defense' in caller_attrs)
+        self.assertTrue('vulnerable' in caller_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' not in caller_attrs)
+
+        # Callee Attributes
+        self.assertIsNotNone(callee_attrs)
+        self.assertTrue('tested' not in callee_attrs)
+        self.assertTrue('defense' in callee_attrs)
+        self.assertTrue('vulnerable' in callee_attrs)
+        self.assertTrue('dangerous' not in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' not in caller_attrs)
+
+        # Scenario: main -- chown (cflow)
+
+        # Arrange
+        source = 'cflow'
+        caller = Call('main', 'main.c', Environments.C)
+        callee = Call('chown', '', Environments.C)
+
+        # Act
+        (caller_attrs, callee_attrs) = utilities.get_node_attrs(
+            source, caller, callee, list(), list()
+        )
+
+        # Assert
+
+        # Caller Attributes
+        self.assertTrue('tested' not in caller_attrs)
+        self.assertTrue('defense' not in caller_attrs)
+        self.assertTrue('vulnerable' not in caller_attrs)
+        self.assertTrue('dangerous' in caller_attrs)
+        self.assertTrue('entry' not in caller_attrs)
+        self.assertTrue('exit' not in caller_attrs)
+
+        # Callee Attributes
+        self.assertIsNone(callee_attrs)
+
+if __name__ == '__main__':
+    unittest.main()
